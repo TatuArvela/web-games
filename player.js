86966c450ccfd89abe54cf1f34c0475f727db17e
Tatu Arvela <tatu.arvela@gmail.com>
2021-10-26 12:28:20 +0300

    Fix emptying iframe

--- player.js
+++ player.js
@@ -2,14 +2,14 @@ const player = document.getElementById("player");
 const iframe = document.getElementById("iframe");
 
 function reset() {
-  player.classList.add('disabled');
-  iframe.src = undefined;
-  iframe.setAttribute("onload", undefined);
+  player.classList.add("disabled");
+  iframe.removeAttribute("onload");
+  iframe.removeAttribute("src");
   document.activeElement.blur();
 }
 
 function play(url) {
-  player.classList.remove('disabled');
+  player.classList.remove("disabled");
   iframe.setAttribute("onload", "iframe.contentWindow.focus()");
-  iframe.src = url;
+  iframe.setAttribute("src", url);
 }
