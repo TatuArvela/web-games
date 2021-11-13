5daa941bf3f25e33258a1c3740971f186601548c
Tatu Arvela <tatu.arvela@gmail.com>
2021-11-13 23:53:10 +0200

    Adjust games data

--- games.js
+++ games.js
@@ -1,44 +1,44 @@
 const games = [
   {
+    title: "Floaty Goat",
+    href: "https://tatuarvela.github.io/codecamp",
+    players: 1,
+    controls: ['mouse', 'keyboard', 'touch'],
+    image: 'img/floaty-goat.png'
+  },
+  {
     title: "Pong",
     href: "games/pong",
     players: 1,
     controls: ['mouse', 'touch'],
-    image: ['img/pong.png']
+    image: 'img/pong.png'
   },
   {
     title: "Snake",
     href: "games/snake",
     players: 1,
     controls: ['keyboard', 'touch'],
-    image: ['img/snake.png'],
+    image: 'img/snake.png',
   },
   {
     title: "Tic-Tac-Toe",
     href: "games/tic-tac-toe",
     players: 2,
     controls: ['mouse', 'touch'],
-    image: ['img/tic-tac-toe.png']
+    image: 'img/tic-tac-toe.png'
   },
   {
     title: "Super Mathgame",
     href: "https://tatuarvela.github.io/Super-Mathgame",
     players: 1,
     controls: ['mouse', 'touch'],
-    image: ['img/super-mathgame.png']
+    image: 'img/super-mathgame.png'
   },
   {
     title: "Sudoku",
     href: "https://tatuarvela.github.io/Sudoku",
     players: 1,
     controls: ['mouse', 'keyboard'],
-    image: ['img/sudoku.png']
-  },
-  {
-    title: "Floaty Goat",
-    href: "https://tatuarvela.github.io/codecamp",
-    players: 1,
-    controls: ['mouse', 'keyboard', 'touch'],
-    image: ['img/floaty-goat.png']
+    image: 'img/sudoku.png'
   },
 ];
