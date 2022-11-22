83eaf6a68c69eb6961ed67eab9cd5382f54e02f2
Tatu Arvela <tatu.arvela@gmail.com>
2022-11-22 22:10:55 +0200

    Add ball collision physics to Marbles

--- games/marbles/marbles.js
+++ games/marbles/marbles.js
@@ -1,32 +1,33 @@
 /*
-* Globals
-* */
+ * Globals
+ * */
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 canvas.width = 800;
 canvas.height = 600;
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 context.imageSmoothingEnabled = false;
 
 const updateRate = 100;
 const frameRate = 60;
 
-const ballSize = 20;
+const ballRadius = 20;
 
-function Ball() {
-  this.x = canvas.width / 2;
-  this.y = canvas.height / 2;
+function Ball(x, y, color) {
+  this.x = x;
+  this.y = y;
   this.speedX = 0;
   this.speedY = 0;
+  this.color = color;
 }
 
 let isMouseDown = false;
 let mouseMovePosition = null;
 
 const maxVectorLength = 50;
-const slowDownRate = 0.0025;
+const slowDownRate = 0.005;
 
-function controlBall() {
+function moveBall(ball) {
   ball.x += ball.speedX;
   ball.y += ball.speedY;
 
@@ -43,22 +44,45 @@ function controlBall() {
   }
 }
 
+function collideBalls(ball1, ball2) {
+  function ballIntersect(ball1, ball2) {
+    return (
+      (ball1.x - ball2.x) * (ball1.x - ball2.x) +
+        (ball1.y - ball2.y) * (ball1.y - ball2.y) <=
+      (ballRadius + ballRadius) * (ballRadius + ballRadius)
+    );
+  }
 
-/*
-* State
-* */
+  if (ballIntersect(ball1, ball2)) {
+    const vCollision = { x: ball2.x - ball1.x, y: ball2.y - ball1.y };
+    const distance = Math.sqrt(
+      (ball2.x - ball1.x) * (ball2.x - ball1.x) +
+        (ball2.y - ball1.y) * (ball2.y - ball1.y)
+    );
+    const vCollisionNorm = {
+      x: vCollision.x / distance,
+      y: vCollision.y / distance,
+    };
+    const vRelativeVelocity = {
+      x: ball1.speedX - ball2.speedX,
+      y: ball1.speedY - ball2.speedY,
+    };
+    const speed =
+      vRelativeVelocity.x * vCollisionNorm.x +
+      vRelativeVelocity.y * vCollisionNorm.y;
 
-let ball = new Ball();
+    if (speed < 0) {
+      return;
+    }
 
+    ball1.speedX -= speed * vCollisionNorm.x;
+    ball1.speedY -= speed * vCollisionNorm.y;
+    ball2.speedX += speed * vCollisionNorm.x;
+    ball2.speedY += speed * vCollisionNorm.y;
+  }
+}
 
-const minX = 0 + ballSize;
-const maxX = canvas.width - ballSize;
-const minY = 0 + ballSize;
-const maxY = canvas.height - ballSize;
-
-function update() {
-  controlBall();
-
+function bounceBallOnEdges(ball) {
   if (ball.x < minX) {
     ball.speedX = ball.speedX * -1;
     ball.x = minX;
@@ -78,15 +102,51 @@ function update() {
   }
 }
 
+/*
+ * State
+ * */
+
+let playerBall = new Ball(canvas.width / 2, canvas.height / 2, "#eee");
+let otherBalls = [
+  new Ball(canvas.width / 4, (1 * canvas.height) / 5, "#48f"),
+  new Ball(canvas.width / 4, (2 * canvas.height) / 5, "#77f"),
+  new Ball(canvas.width / 4, (3 * canvas.height) / 5, "#94f"),
+  new Ball(canvas.width / 4, (4 * canvas.height) / 5, "#b4e"),
+  new Ball((3 * canvas.width) / 4, (1 * canvas.height) / 5, "#c59"),
+  new Ball((3 * canvas.width) / 4, (2 * canvas.height) / 5, "#d64"),
+  new Ball((3 * canvas.width) / 4, (3 * canvas.height) / 5, "#c80"),
+  new Ball((3 * canvas.width) / 4, (4 * canvas.height) / 5, "#ba0"),
+];
+
+const minX = 0 + ballRadius;
+const maxX = canvas.width - ballRadius;
+const minY = 0 + ballRadius;
+const maxY = canvas.height - ballRadius;
+
+function update() {
+  moveBall(playerBall);
+  otherBalls.forEach((ball) => moveBall(ball));
+  const allBalls = [playerBall, ...otherBalls];
+  for (let i = 0; i < allBalls.length; i++) {
+    for (let j = 0; j < allBalls.length; j++) {
+      if (i !== j) {
+        collideBalls(allBalls[i], allBalls[j]);
+      }
+    }
+  }
+  bounceBallOnEdges(playerBall);
+  otherBalls.forEach((ball) => bounceBallOnEdges(ball));
+}
+
 function drawGrass() {
-  context.fillStyle = '#22dd22';
+  context.fillStyle = "#060";
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawBall() {
+function drawBall(ball) {
   context.beginPath();
-  context.arc(ball.x, ball.y, ballSize, 0, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.arc(ball.x, ball.y, ballRadius, 0, 2 * Math.PI, false);
+  context.fillStyle = ball.color;
   context.fill();
 }
 
@@ -96,16 +156,10 @@ function getAbsDistance(a, b) {
 
 function drawLine() {
   context.beginPath();
-  context.moveTo(ball.x, ball.y);
-  context.strokeStyle = 'blue';
+  context.moveTo(playerBall.x, playerBall.y);
+  context.strokeStyle = "blue";
   context.lineWidth = 5;
   const [x, y] = mouseMovePosition;
-
-  // TODO
-  // const xDistance = getAbsDistance(ball.x, x);
-  // const yDistance = getAbsDistance(ball.y, y);
-  // const vectorLength = (xDistance + yDistance) / 2;
-  // const scale = 1;
 
   context.lineTo(x, y);
   context.stroke();
@@ -116,7 +170,8 @@ function draw() {
   if (isMouseDown) {
     drawLine();
   }
-  drawBall();
+  drawBall(playerBall);
+  otherBalls.forEach((ball) => drawBall(ball));
 }
 
 setInterval(() => update(), 1000 / updateRate);
@@ -130,18 +185,27 @@ function getCanvasEventPosition(event) {
   return [x, y];
 }
 
-canvas.addEventListener('mousedown', function (downEvent) {
+canvas.addEventListener("mousedown", function (downEvent) {
   isMouseDown = true;
   mouseMovePosition = getCanvasEventPosition(downEvent);
-  const mousemoveEventListener = document.addEventListener('mousemove', function (moveEvent) {
-    mouseMovePosition = getCanvasEventPosition(moveEvent);
-  });
-  document.addEventListener('mouseup', function () {
-    isMouseDown = false;
-    const xSign = ball.x > mouseMovePosition[0] ? 1 : -1;
-    const ySign = ball.y > mouseMovePosition[1] ? 1 : -1;
-    ball.speedX += xSign * getAbsDistance(ball.x, mouseMovePosition[0]) / 10;
-    ball.speedY += ySign * getAbsDistance(ball.y, mouseMovePosition[1]) / 10;
-    document.removeEventListener('mousemove', mousemoveEventListener);
-  }, { once: true })
+  const mousemoveEventListener = document.addEventListener(
+    "mousemove",
+    function (moveEvent) {
+      mouseMovePosition = getCanvasEventPosition(moveEvent);
+    }
+  );
+  document.addEventListener(
+    "mouseup",
+    function () {
+      isMouseDown = false;
+      const xSign = playerBall.x > mouseMovePosition[0] ? 1 : -1;
+      const ySign = playerBall.y > mouseMovePosition[1] ? 1 : -1;
+      playerBall.speedX +=
+        (xSign * getAbsDistance(playerBall.x, mouseMovePosition[0])) / 10;
+      playerBall.speedY +=
+        (ySign * getAbsDistance(playerBall.y, mouseMovePosition[1])) / 10;
+      document.removeEventListener("mousemove", mousemoveEventListener);
+    },
+    { once: true }
+  );
 });
