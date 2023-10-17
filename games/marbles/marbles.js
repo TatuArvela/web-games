64710bd6e68487398750c3112d6393ef0d83f76b
Tatu Arvela <tatu.arvela@gmail.com>
2023-10-18 00:18:38 +0300

    marbles: progress on game concept and mechanics

--- games/marbles/marbles.js
+++ games/marbles/marbles.js
@@ -8,71 +8,79 @@ canvas.height = 600;
 const context = canvas.getContext("2d");
 context.imageSmoothingEnabled = false;
 
+const boardWidth = 600;
+const boardHeight = 400;
+
 const updateRate = 100;
-const frameRate = 60;
-
-const ballRadius = 12;
-
-let nextIdentifier = 1;
-function getNextIdentifier() {
-  return nextIdentifier && nextIdentifier++;
-}
-
-function Ball(x, y, color, identifier) {
-  this.identifier = identifier ?? getNextIdentifier();
-  this.x = x;
-  this.y = y;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.color = color;
-  this.touched = false;
-}
-
-let isMouseDown = false;
-let mouseMovePosition = null;
-
 const maxVectorLength = 50;
 const slowDownRate = 0.005;
 
-function moveBall(ball) {
-  ball.x += ball.speedX;
-  ball.y += ball.speedY;
-
-  if (Math.abs(ball.speedX) < 0.1) {
-    ball.speedX = 0;
-  } else {
-    ball.speedX = ball.speedX * (1 - slowDownRate);
-  }
-
-  if (Math.abs(ball.speedY) < 0.1) {
-    ball.speedY = 0;
-  } else {
-    ball.speedY = ball.speedY * (1 - slowDownRate);
+class Marble {
+  constructor(x, y, color, id, controllable) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.color = color;
+    this.controllable = controllable;
   }
 }
 
-function collideBalls(ball1, ball2) {
-  function ballIntersect(ball1, ball2) {
-    return (
-      (ball1.x - ball2.x) * (ball1.x - ball2.x) +
-        (ball1.y - ball2.y) * (ball1.y - ball2.y) <=
-      (ballRadius + ballRadius) * (ballRadius + ballRadius)
-    );
+/*
+ * Mechanics
+ * */
+
+function updateCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function getBoardOffsetX() {
+  return (canvas.width - boardWidth) / 2;
+}
+
+function getBoardOffsetY() {
+  return (canvas.height - boardHeight) / 2;
+}
+
+function moveMarble(marble) {
+  marble.x += marble.speedX;
+  marble.y += marble.speedY;
+
+  if (Math.abs(marble.speedX) < 0.1) {
+    marble.speedX = 0;
+  } else {
+    marble.speedX = marble.speedX * (1 - slowDownRate);
   }
 
-  if (ballIntersect(ball1, ball2)) {
-    const vCollision = { x: ball2.x - ball1.x, y: ball2.y - ball1.y };
+  if (Math.abs(marble.speedY) < 0.1) {
+    marble.speedY = 0;
+  } else {
+    marble.speedY = marble.speedY * (1 - slowDownRate);
+  }
+}
+
+function checkMarbleIntersect(marble1, marble2) {
+  let dx = marble1.x - marble2.x;
+  let dy = marble1.y - marble2.y;
+  return Math.sqrt(dx * dx + dy * dy) < 2 * marbleRadius;
+}
+
+function collideMarbles(marble1, marble2) {
+  if (checkMarbleIntersect(marble1, marble2)) {
+    const vCollision = { x: marble2.x - marble1.x, y: marble2.y - marble1.y };
     const distance = Math.sqrt(
-      (ball2.x - ball1.x) * (ball2.x - ball1.x) +
-        (ball2.y - ball1.y) * (ball2.y - ball1.y)
+      (marble2.x - marble1.x) * (marble2.x - marble1.x) +
+        (marble2.y - marble1.y) * (marble2.y - marble1.y)
     );
     const vCollisionNorm = {
       x: vCollision.x / distance,
       y: vCollision.y / distance,
     };
     const vRelativeVelocity = {
-      x: ball1.speedX - ball2.speedX,
-      y: ball1.speedY - ball2.speedY,
+      x: marble1.speedX - marble2.speedX,
+      y: marble1.speedY - marble2.speedY,
     };
     const speed =
       vRelativeVelocity.x * vCollisionNorm.x +
@@ -82,87 +90,173 @@ function collideBalls(ball1, ball2) {
       return;
     }
 
-    ball1.speedX -= speed * vCollisionNorm.x;
-    ball1.speedY -= speed * vCollisionNorm.y;
-    ball2.speedX += speed * vCollisionNorm.x;
-    ball2.speedY += speed * vCollisionNorm.y;
+    marble1.speedX -= speed * vCollisionNorm.x;
+    marble1.speedY -= speed * vCollisionNorm.y;
+    marble2.speedX += speed * vCollisionNorm.x;
+    marble2.speedY += speed * vCollisionNorm.y;
 
-    ball1.touched = true;
-    ball2.touched = true;
+    marble1.controllable = true;
+    marble2.controllable = true;
   }
 }
 
-function bounceBallOnEdges(ball) {
-  if (ball.x < minX) {
-    ball.speedX = ball.speedX * -1;
-    ball.x = minX;
-  }
-  if (ball.x > maxX) {
-    ball.speedX = ball.speedX * -1;
-    ball.x = maxX;
+function asHexColor(r, g, b) {
+  const rString = r.toString(16).padStart(2, "0");
+  const gString = g.toString(16).padStart(2, "0");
+  const bString = b.toString(16).padStart(2, "0");
+  return `#${rString}${gString}${bString}`;
+}
+
+function generateDistinctBrightColor() {
+  const ceil = 255;
+  const floor = 128;
+  const colorStep = 5;
+
+  const channelMax = ceil;
+  const channelMid =
+    Math.floor(Math.random() * ((ceil - floor) / colorStep)) * colorStep +
+    floor;
+  const channelMin = floor;
+
+  const whichChannel = Math.floor(Math.random() * 3);
+
+  let r, g, b;
+  switch (whichChannel) {
+    case 0:
+      r = channelMax;
+      g = channelMid;
+      b = channelMin;
+      break;
+    case 1:
+      r = channelMin;
+      g = channelMax;
+      b = channelMid;
+      break;
+    case 2:
+      r = channelMid;
+      g = channelMin;
+      b = channelMax;
+      break;
   }
 
-  if (ball.y < minY) {
-    ball.speedY = ball.speedY * -1;
-    ball.y = minY;
+  return asHexColor(r, g, b);
+}
+
+function generateMarbles(numMarbles, boardWidth, boardHeight, marbleRadius) {
+  let marbles = [];
+  let attempts = 0;
+
+  for (let i = 0; i < numMarbles; i++) {
+    let overlap = false;
+    let newMarble;
+
+    do {
+      let x = Math.random() * (boardWidth - 2 * marbleRadius) + marbleRadius;
+      let y = Math.random() * (boardHeight - 2 * marbleRadius) + marbleRadius;
+      let color = generateDistinctBrightColor();
+
+      newMarble = new Marble(x, y, color, i, i === 0);
+
+      overlap = marbles.some((marble) =>
+        checkMarbleIntersect(marble, newMarble)
+      );
+
+      attempts++;
+
+      if (attempts > 1000) {
+        throw new Error("Too many attempts to place non-overlapping marbles.");
+      }
+    } while (overlap);
+
+    marbles.push(newMarble);
   }
-  if (ball.y > maxY) {
-    ball.speedY = ball.speedY * -1;
-    ball.y = maxY;
-  }
+
+  return marbles;
 }
 
 /*
  * State
  * */
 
-let balls = [
-  new Ball(canvas.width / 6, canvas.height / 2, "#eee", "player"),
-  new Ball(canvas.width / 4, (2 * canvas.height) / 7, "#77f"),
-  new Ball(canvas.width / 4, (3 * canvas.height) / 7, "#94f"),
-  new Ball(canvas.width / 4, (4 * canvas.height) / 7, "#b4e"),
-  new Ball(canvas.width / 4, (5 * canvas.height) / 7, "#48f"),
-  new Ball((2 * canvas.width) / 4, (2 * canvas.height) / 7, "#ba0"),
-  new Ball((2 * canvas.width) / 4, (3 * canvas.height) / 7, "#ba0"),
-  new Ball((2 * canvas.width) / 4, (4 * canvas.height) / 7, "#ba0"),
-  new Ball((2 * canvas.width) / 4, (5 * canvas.height) / 7, "#ba0"),
-  new Ball((3 * canvas.width) / 4, (2 * canvas.height) / 7, "#d64"),
-  new Ball((3 * canvas.width) / 4, (3 * canvas.height) / 7, "#c80"),
-  new Ball((3 * canvas.width) / 4, (4 * canvas.height) / 7, "#ba0"),
-  new Ball((3 * canvas.width) / 4, (5 * canvas.height) / 7, "#c59"),
-];
-function getPlayerBall() {
-  return balls.find((ball) => ball.identifier === "player");
+let marbleRadius = 12;
+let numMarbles = 50;
+let marbles = [];
+
+let isMouseDown = false;
+let mouseMovePosition = null;
+
+function getPlayerMarble() {
+  return marbles[0];
 }
 
-const minX = 0 + ballRadius;
-const maxX = canvas.width - ballRadius;
-const minY = 0 + ballRadius;
-const maxY = canvas.height - ballRadius;
+function initialize() {
+  marbles = generateMarbles(numMarbles, boardWidth, boardHeight, marbleRadius);
+}
 
 function update() {
-  balls.forEach((ball) => {
-    moveBall(ball);
+  marbles.forEach((marble) => {
+    moveMarble(marble);
   });
-  for (let i = 0; i < balls.length; i++) {
-    for (let j = 0; j < balls.length; j++) {
+  for (let i = 0; i < marbles.length; i++) {
+    for (let j = 0; j < marbles.length; j++) {
       if (i !== j) {
-        collideBalls(balls[i], balls[j]);
+        collideMarbles(marbles[i], marbles[j]);
       }
     }
   }
-  balls.forEach((ball) => bounceBallOnEdges(ball));
 }
 
-function drawGrass() {
-  context.fillStyle = "#060";
+function clearCanvas() {
+  context.fillStyle = "#808080";
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawBall(ball) {
+function drawBoard() {
+  const stripeWidth = 20;
+  const stripeColor1 = "#ddd";
+  const stripeColor2 = "#ccc";
+  const angleInDegrees = 45;
+  const angleInRadians = (Math.PI / 180) * angleInDegrees;
+
+  context.save();
+
+  context.translate(getBoardOffsetX(), getBoardOffsetY());
+  context.rect(0, 0, boardWidth, boardHeight);
+  context.clip();
+
+  context.fillStyle = stripeColor1;
+  context.fillRect(0, 0, boardWidth, boardHeight);
+
+  context.fillStyle = stripeColor2;
+
+  context.rotate(angleInRadians);
+
+  const rotatedWidth =
+    boardWidth * Math.abs(Math.cos(angleInRadians)) +
+    boardHeight * Math.abs(Math.sin(angleInRadians));
+  const rotatedHeight =
+    boardHeight * Math.abs(Math.cos(angleInRadians)) +
+    boardWidth * Math.abs(Math.sin(angleInRadians));
+  const length = Math.max(rotatedWidth, rotatedHeight);
+
+  for (let i = -length; i < length; i += stripeWidth * 2) {
+    context.fillRect(i, -length, stripeWidth, length * 2);
+  }
+
+  context.restore();
+}
+
+function drawMarble(marble) {
   context.beginPath();
-  context.arc(ball.x, ball.y, ballRadius, 0, 2 * Math.PI, false);
-  context.fillStyle = ball.touched ? "white" : ball.color;
+  context.arc(
+    marble.x + getBoardOffsetX(),
+    marble.y + getBoardOffsetY(),
+    marbleRadius,
+    0,
+    2 * Math.PI,
+    false
+  );
+  context.fillStyle = marble.controllable ? "white" : marble.color;
   context.fill();
 }
 
@@ -171,28 +265,47 @@ function getAbsDistance(a, b) {
 }
 
 function drawLine() {
-  const playerBall = getPlayerBall();
+  const playerMarble = getPlayerMarble();
+
   context.beginPath();
-  context.moveTo(playerBall.x, playerBall.y);
+  context.moveTo(
+    playerMarble.x + getBoardOffsetX(),
+    playerMarble.y + getBoardOffsetY()
+  );
+
   context.strokeStyle = "blue";
   context.lineWidth = 3;
+
   const [x, y] = mouseMovePosition;
+
   context.lineTo(x, y);
   context.stroke();
 }
 
 function draw() {
-  drawGrass();
-  if (isMouseDown && !getPlayerBall().touched) {
+  clearCanvas()
+  drawBoard();
+
+  marbles.forEach((marble) => {
+    drawMarble(marble);
+  });
+
+  if (isMouseDown) {
     drawLine();
   }
-  balls.forEach((ball) => {
-    drawBall(ball);
-  });
+
+  requestAnimationFrame(draw);
 }
 
+/*
+ * Initialisation
+ */
+
+initialize();
+
+updateCanvasSize();
 setInterval(() => update(), 1000 / updateRate);
-setInterval(() => draw(), 1000 / frameRate);
+requestAnimationFrame(draw);
 
 function getCanvasEventPosition(event) {
   const rect = canvas.getBoundingClientRect();
@@ -202,29 +315,44 @@ function getCanvasEventPosition(event) {
   return [x, y];
 }
 
+document.addEventListener('resize', function () {
+  updateCanvasSize();
+});
+
 canvas.addEventListener("mousedown", function (downEvent) {
-  if (getPlayerBall().touched) {
-    return;
-  }
   isMouseDown = true;
   mouseMovePosition = getCanvasEventPosition(downEvent);
+
   const mousemoveEventListener = document.addEventListener(
     "mousemove",
     function (moveEvent) {
       mouseMovePosition = getCanvasEventPosition(moveEvent);
     }
   );
+
   document.addEventListener(
     "mouseup",
     function () {
       isMouseDown = false;
-      const playerBall = getPlayerBall();
-      const xSign = playerBall.x > mouseMovePosition[0] ? 1 : -1;
-      const ySign = playerBall.y > mouseMovePosition[1] ? 1 : -1;
-      playerBall.speedX +=
-        (xSign * getAbsDistance(playerBall.x, mouseMovePosition[0])) / 10;
-      playerBall.speedY +=
-        (ySign * getAbsDistance(playerBall.y, mouseMovePosition[1])) / 10;
+      const playerMarble = getPlayerMarble();
+      const xSign =
+        playerMarble.x + getBoardOffsetX() > mouseMovePosition[0] ? 1 : -1;
+      const ySign =
+        playerMarble.y + getBoardOffsetY() > mouseMovePosition[1] ? 1 : -1;
+      playerMarble.speedX +=
+        (xSign *
+          getAbsDistance(
+            playerMarble.x + getBoardOffsetX(),
+            mouseMovePosition[0]
+          )) /
+        10;
+      playerMarble.speedY +=
+        (ySign *
+          getAbsDistance(
+            playerMarble.y + getBoardOffsetY(),
+            mouseMovePosition[1]
+          )) /
+        10;
       document.removeEventListener("mousemove", mousemoveEventListener);
     },
     { once: true }
@@ -232,11 +360,9 @@ canvas.addEventListener("mousedown", function (downEvent) {
 });
 
 // # TODO
-// Generate random ball positions
+// Allow flicking all controllable marbles
+// Falling off the board
 // Count and show score
 // Restart button
-// Allow multiple moves
-// Level system (increase amount of balls, decrease amount of moves)
-// Random, unique ball colors
-// Allow starting move from any ball
+// Level system (change number of marbles, change size of board)
 // "Level Complete!" splash
